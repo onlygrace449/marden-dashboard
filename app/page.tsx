bcfc5772bb30d0fbc708fe0d4203f631dc933583
graceonly449 <graceonly449@gmail.com>
2024-04-01 00:18:19 +0800

    Update SEO layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+import MardenLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-orange-400 p-4 md:h-52">
-        <AcmeLogo />
+        <MardenLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -16,10 +16,13 @@ export default function Page() {
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
             <strong>Welcome to Marden.</strong>
-            <a href="https://nextjs.org/learn/" className="text-orange-500">
+            <a
+              href="http://marden-dashboard.vercel.app"
+              className="text-orange-500"
+            >
               Admin Dashborad
             </a>
-            , brought to you by AET Group.
+            , brought to you by EAT Team.
           </p>
           <Link
             href="/login"
